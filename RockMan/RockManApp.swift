1253d1a2ac4e4366e7575fa8d404a43e5fd67d84
keshew <artyom.korotckov2017@yandex.ru>
2024-12-03 22:59:47 +0500

    Initial Commit

---
+++ RockMan/RockManApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockManApp.swift
+//  RockMan
+//
+//  Created by Артём Коротков on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
